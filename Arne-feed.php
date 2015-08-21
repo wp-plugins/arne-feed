082e9cde994ee8ff7fa1a3d128b5ee8ecdcb7391
Alireza Nejati <Alireza Nejati@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-21 11:42:10 +0000

    update 1.2
    git-svn-id: https://plugins.svn.wordpress.org/arne-feed/trunk@1227136 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Arne-feed.php
+++ Arne-feed.php
@@ -4,9 +4,9 @@ Plugin Name: Arne Feed
 Plugin URI: http://arneweb.ir
 Description: Show Last posts of <a target=_blank" href="http://Arneweb.ir">Arneweb.ir</a> in your dashboard.
 Author: Alireza Nejati
-Version: 1.1
+Version: 1.2
 Tags: feed, Arneweb, Arneweb feed, wordpress
-Author URI: http://arnejati.ir
+Author URI: http://alirezanejati.ir
 License: GPL
 */
 // Add Arneweb.ir feed //
@@ -40,4 +40,22 @@ function Arne_action_init() {
 load_plugin_textdomain('Arne', false, dirname(plugin_basename(__FILE__) ) . '/lang/' );
 }
 add_action('init', 'Arne_action_init');
+// start //
+/** Step 2 (from text above). */
+add_action( 'admin_menu', 'Arne_Feed_menu' );
+
+/** Step 1. */
+function Arne_Feed_menu() {
+	add_options_page( 'Arne Feed Options', 'Arne Feed', 'manage_options', 'my-unique-identifier', 'Arne_Feed_options' );
+}
+
+/** Step 3. */
+function Arne_Feed_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.', 'Arne' ) );
+	}
+	echo '<div class="wrap">';
+	echo '<p>Upgrading ...</p>';
+	echo '</div>';
+}
 ?>
